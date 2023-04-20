90cf66e50714f31f303495e2d0b14d515505ec30
Nikita Argunov <chichiletme@gmail.com>
2023-04-20 20:41:14 +0300

    added html css img elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
+import "./index.scss";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>привет</p>
-        <a className="App-link" href="#" target="_blank">
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="header">
+        <div className="header__nav">
+          <img
+            className="header__logo"
+            width={40}
+            height={40}
+            src="/img/logo.svg"
+            alt="logo"
+          />
+
+          <div className="header__info">
+            <h3 className="header__title">R Sneakers</h3>
+
+            <p className="header__subtitle">Нормальный магазин кроссовок</p>
+          </div>
+        </div>
+
+        <ul className="header__list">
+          <li className="header__list-item">
+            <img width={18} height={18} src="/img/cart.svg" alt="cart" />
+
+            <span>1205 руб.</span>
+          </li>
+
+          <li className="header__list-item">
+            <img width={18} height={18} src="/img/user.svg" alt="user" />
+          </li>
+        </ul>
       </header>
+
+      <div className="content">
+        <h2 className="content__title">Все кроссовки</h2>
+
+        <div className="card">
+          <img src="/img/sneakers/1.jpg" alt="sneaker" />
+          <p></p>
+          <div>
+            <span>Цена:</span>
+            <b>12 999 р.</b>
+            <button>
+              <img width={11} height={11} src="/img/plus.svg" alt="plus" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
